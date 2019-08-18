2f4266d464bedaef3ecf5eaf6a30d13e010ed432
Aref <tabideh2005@gmail.com>
2019-08-18 08:28:51 -0700

    add

--- 7/main.c
+++ 7/main.c
@@ -4,5 +4,57 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+
+int p[10];
+
+p[0]=175;
+p[1]=184;
+p[2]=181;
+p[3]=189;
+p[4]=173;
+p[5]=178;
+p[6]=171;
+p[7]=176;
+p[8]=173;
+p[9]=190;
+
+for() {
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
